8fa15ec83d5947f50154e3bd5e7d99ca02291d13
alexandr.dorofeev <alzumar91@mail.ru>
2017-06-27 11:44:18 +0300

    Update todo.js

--- NodeJS-Command-Line-TODO/todo.js
+++ NodeJS-Command-Line-TODO/todo.js
@@ -2,6 +2,7 @@ let R = require("ramda")
 let dbFile = "./todo.json"
 let FS = require("fs-extra")
 
+
 //parse init
 
 let operation = process.argv[2]  //operation with task
@@ -11,35 +12,77 @@ let task = process.argv[3]  //task description
 
 let commands = {}
 
-commands.init = function () {
-  FS.writeFileSync(dbFile, "[]")
-}
-
-/*let writeFileX = R.curry((file, content) => {
-  let str = typeof content == "string" ? content : JSON.stringify(content, null, 2)
-  FS.outputFileSync(file, str, "utf-8")
-  return content
+/*let writeFileX = R.curry((dbFile, todo) => {
+  let str = typeof todo == "string" ? todo : JSON.stringify(todo, null, 2)
+  FS.outputFileSync(dbFile, str, "utf-8")
+  return todo
 })*/
 
-commands.add = function (todo) {
+commands.init = function () {
+  let todos = []
+  let todosStr = JSON.stringify(tasks, null, 2)
+  FS.writeFileSync(dbFile, todosStr)
+  console.log(todosStr)
+}
+
+commands.add = function (text) {
   let todos = require(dbFile)
-  FS.writeFileSync(dbFile, JSON.stringify(R.append(todo, todos), null, 2))
+  let todos2 = R.append({text, done: false, archived: false}, todos)
+  let todos2Str = JSON.stringify(todos2, null, 2)
+  FS.writeFileSync(dbFile, todos2Str)
+  console.log(text)
+  let recentTodos = R.filter(x => !x.archived, todos)
+  forEachI(
+    (todo, index) => console.log(index + 1, todo.done ? '[x]' : '[ ]', todo.text),
+    recentTodos
+  )
+}
+
+let forEachI = R.addIndex(R.forEach)
+
+commands.list = function () {
+  let todos = require(dbFile)
+  let recentTodos = R.filter(x => !x.archived, todos)
+  forEachI(
+    (todo, index) => console.log(index + 1, todo.done ? '[x]' : '[ ]', todo.text),
+    recentTodos
+  )
 }
 
 commands.delete = function (index) {
+  index = Number(index)
   let todos = require(dbFile)
-  let todos1 = R.remove(index, 1, todos)
-  let str = JSON.stringify(todos1, null, 2)
-  FS.writeFileSync(dbFile, str);
-  console.log(str)
+  let todos2 = R.remove(index, 1, todos)
+  let todos2Str = JSON.stringify(todos2, null, 2)
+  FS.writeFileSync(dbFile, todos2Str)
+  let recentTodos = R.filter(x => !x.archived, todos2)
+  forEachI(
+    (todo, index) => console.log(index + 1, todo.done ? '[x]' : '[ ]', todo.text),
+    recentTodos
+  )
 }
 
 commands.done = function (index) {
+  index = Number(index)
   let todos = require(dbFile)
   let todos2 = R.update(index, R.assoc("done", true, todos[index]), todos)
-  let str1 = JSON.stringify(todos2, null, 2)
-  FS.writeFileSync(dbFile, str1);
-  console.log(str1)
+  let todos2Str = JSON.stringify(todos2, null, 2)
+  FS.writeFileSync(dbFile, todos2Str)
+  let recentTodos = R.filter(x => !x.archived, todos2)
+  forEachI(
+    (todo, index) => console.log(index + 1, todo.done ? '[x]' : '[ ]', todo.text),
+    recentTodos
+  )
+}
+
+commands.archive = function () {
+  let todos = require(dbFile)
+  let todos2 = R.map(R.assoc("archived", true), todos)
+  /*let todos2 = R.map(function() {
+    return R.assoc("archived", true, todos[0])
+  }, todos)*/
+  let todos2Str = JSON.stringify(todos2, null, 2)
+  FS.writeFileSync(dbFile, todos2Str)
 }
 
 //command manager
@@ -48,16 +91,19 @@ switch (operation) {
   case "init":
     return commands.init()
   case "add":
-    return commands.add({
-      task,
-      done: false,
-      archived: false,
-    })
+    //let text = process.argv[3]
+    return commands.add(task)
+  case "list":
+    return commands.list()
   case "delete":
+    //let index = Number(process.argv[3])
     return commands.delete(task)
   case "done":
+    //let index = Number(process.argv[3])
     return commands.done(task)
-    break;
+  case "archive":
+    //let index = Number(process.argv[3])
+    return commands.archive()
   default:
-
+    throw Exception(`unsupported operation ${operation}`)
 }
